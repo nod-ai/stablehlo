a65c604c34e8afd05523d19a1641fcfb84f8a81b
Boian Petkantchin <boian@nod-labs.com>
2023-08-22 18:01:58 -0700

    Add pass stablehlo-move-device-domain-from-frontend-attributes

--- stablehlo/transforms/collectives/CollectivesPasses.cpp
+++ stablehlo/transforms/collectives/CollectivesPasses.cpp
@@ -1,5 +1,6 @@
 #include "CollectivesPassesCliImpl.h"
 #include "CollectivesSpmdSubPartitionerImpl.h"
 #include "CompleteCollectivesSpmdSubPartitionImpl.h"
+#include "MoveDeviceDomainFromFrontendAttributesImpl.h"
 #include "MoveDeviceDomainToFrontendAttributesImpl.h"
 #include "SetCollectivesDeviceDomainImpl.h"
